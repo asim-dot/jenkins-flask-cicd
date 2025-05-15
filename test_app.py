04b9bd4d5cc3c6675e9299b1108b6af4c674426d
Abdul Asim <masim227@gmail.com>
2025-05-15 17:02:52 +0530

    Add about page

--- test_app.py
+++ test_app.py
@@ -17,3 +17,8 @@ def test_api_status(client):
     assert response.status_code == 200
     json_data = response.get_json()
     assert json_data['status'] == 'OK'
+
+def test_about_page(client):
+    response = client.get('/about')
+    assert response.status_code == 200
+    assert b'About This App' in response.data
